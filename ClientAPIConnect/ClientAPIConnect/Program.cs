197be69c4c46fe258bdfe44d719f0e564aecad88
ChybaYg <84780393+ChybaYg@users.noreply.github.com>
2022-08-01 16:45:20 +0200

    Adding the files i am typing about

---
+++ ClientAPIConnect/ClientAPIConnect/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientAPIConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string allUsedMethods = String.Empty;
+            //allUsedMethods = "asikToJedeSefe";
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+            using (StreamReader reader = new StreamReader("WorkingDirectory\\"+args[0]))
+            {
+                allUsedMethods = reader.ReadToEnd();
+            }
+            Client.StartClient(allUsedMethods);
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ Listener.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AnalysingToolForFuzzer.Models
+{
+    public class Listener
+    {
+        // Thread signal.  
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        public static string transferedContent = String.Empty;
+
+        public Listener()
+        {
+        }
+
+        public static void StartListening()
+        {
+            IPAddress ipAddress = IPAddress.Loopback;
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+                bool endOfCycle = false;
+                while (!endOfCycle)
+                {
+                    if (transferedContent != String.Empty)
+                    {                        
+                        endOfCycle = true;
+                        continue;
+                    }
+
+                    allDone.Reset();
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback),
+                        listener);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+
+
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                state.sb.Append(Encoding.ASCII.GetString(
+                    state.buffer, 0, bytesRead));
+                content = state.sb.ToString();
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    content.Replace("<EOF>", "");
+                    transferedContent = content;
+                    Console.WriteLine("Succesfully got Assembly info from tested dlms server");
+                    Send(handler, "200 OK");
+                }
+                else
+                {
+                    // Not all data received. Get more.  
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                        new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.  
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+        private static void Send(Socket handler, byte[] data)
+        {
+            // Convert the string data to byte data using ASCII e
+            // Begin sending the data to the remote device.  
+            handler.BeginSend(data, 0, data.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.  
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    }
+}
